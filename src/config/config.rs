59597a6589dee2c0f9f99017c995310abdc3b570
sofiworker <sofiworker@outlook.com>
2024-01-15 22:56:51 +0800

    fix:调整格式

--- src/config/config.rs
+++ src/config/config.rs
@@ -2,7 +2,25 @@ use serde_derive::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    name: String,
-    age: u32,
-    email: Option<String>,
+    net: NetConfig,
+    service: ServiceConfig,
+    log: LogConfig,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct NetConfig {
+    bpf_filter: String,
+    link_name: Vec<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ServiceConfig {
+    host: String,
+    port: u32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct LogConfig {
+    level: String,
+    output: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use std::{fs::{read, File}, io::Read};
+use config::config as conf;
+use std::{
+    fs::{read, File},
+    io::Read,
+};
 use toml;
-use config::config as c;
 
 pub mod config;
 pub mod services;
 
 fn main() {
-    let mut config_file = File::open("config.toml").expect("can't open config file");
-    let mut content = String::new();
-    config_file.read_to_string(&mut content).expect("read config to string failed");
-    let conf: c::Config = toml::from_str(&content).expect("无法解析TOML文件");
-    println!("{:?}", conf);
+    let mut config_file = File::open("/root/workspace/rust/arkime-rust/src/config.toml")
+        .expect("can't open config file");
+    let mut contents = String::new();
+    config_file
+        .read_to_string(&mut contents)
+        .expect("read config to string failed");
+    let conf: conf::Config = toml::from_str(&contents).unwrap();
+    println!("{:#?}", conf);
 }
