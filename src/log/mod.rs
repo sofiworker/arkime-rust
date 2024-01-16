e82c621bda17345947b18444370ae16263667df0
sofiworker <sofiworker@outlook.com>
2024-01-16 14:25:49 +0800

    add tokio

---
+++ src/log/mod.rs
@@ -0,0 +1,11 @@
+pub trait Log {
+    fn debug(fmt: String);
+    fn info(fmt: String);
+    fn warn(fmt: String);
+    fn error(fmt: String);
+    fn panic(fmt: String);
+}
+
+pub struct LogInstance {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use config::config as conf;
 use std::{
-    fs::{read, File},
+    fs::{File},
     io::Read,
 };
 use toml;
 
 pub mod config;
-pub mod services;
+pub mod service;
+pub mod log;
 
+#[tokio::main]
 fn main() {
-    let mut config_file = File::open("/root/workspace/rust/arkime-rust/src/config.toml")
+    let config_path = String::from("src/config.toml");
+    let mut config_file = File::open(config_path)
         .expect("can't open config file");
     let mut contents = String::new();
     config_file
@@ -17,4 +20,5 @@ fn main() {
         .expect("read config to string failed");
     let conf: conf::Config = toml::from_str(&contents).unwrap();
     println!("{:#?}", conf);
+
 }
